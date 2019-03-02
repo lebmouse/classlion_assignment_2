10168eca82343d0cac13179ffee7154dbf409f79
lebmouse <gtah2yk@gmail.com>
2019-03-02 16:45:27 +0900

    heroku first deploy

--- lionblog/settings.py
+++ lionblog/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*)pvm0vu6e=ojg8k9mfos%$6-a%kgeh(2#l!e98$oj+3crlu(g'
+# SECRET_KEY = '*)pvm0vu6e=ojg8k9mfos%$6-a%kgeh(2#l!e98$oj+3crlu(g'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','*)pvm0vu6e=ojg8k9mfos%$6-a%kgeh(2#l!e98$oj+3crlu(g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
